e67c2472672502ef55964950792481e1b6a083b8
Sushmit Kumar Satapathy <sushmitkumarsatapathy@gmail.com>
2026-06-03 00:14:43 +0530

    Fixing FrontEnd Error

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <Routes>
       {/* Public Landing & Login (Only accessible when NOT logged in) */}
+      <Route path="/test" element={<div style={{ padding: '50px', color: 'red', fontSize: '30px' }}>Router is Working!</div>} />
       <Route 
         path="/" 
         element={
